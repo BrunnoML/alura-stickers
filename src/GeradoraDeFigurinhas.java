b6a1cbdcf7e34b80a3a9cf8dc156915bad7acd04
brunnoml <brunnoml@gmail.com>
2023-04-04 15:54:47 -0300

    feature: put contour outline

--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,8 +1,13 @@
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
+import java.awt.BasicStroke;
 //import java.net.URL;
 import java.awt.Color;
 import java.awt.Font;
@@ -46,7 +51,26 @@ public class GeradoraDeFigurinhas {
     Rectangle2D retangulo = fontMetrics.getStringBounds(texto, graphics);
     int larguraTexto = (int) retangulo.getWidth();
     int posicaoTextoX = (largura - larguraTexto) / 2;
+    int posicaoTextoY = novaAltura - 100;
     graphics.drawString(texto, posicaoTextoX, novaAltura -100);
+
+    //adicionar contorno na imagem
+    FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+    var textLayout = new TextLayout(texto, fonte, fontRenderContext);
+
+    Shape outline = textLayout.getOutline(null);
+    AffineTransform transform = graphics.getTransform();
+    transform.translate(posicaoTextoX, posicaoTextoY);
+    graphics.setTransform(transform);
+
+    var outlineStroke = new BasicStroke(largura * 0.004f);
+    graphics.setStroke(outlineStroke);
+
+    graphics.setColor(Color.BLACK);
+    graphics.draw(outline);
+    graphics.setClip(outline);
+
+
 
     // escrever a nova imagem em um arquivo para pasta no computador
    // ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
